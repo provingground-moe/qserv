8e0fca72d2f9209353e006733ef9412b05c28836
Igor Gaponenko <gapon@slac.stanford.edu>
2018-02-15 07:13:37 +0000

    a prototype of the client-side API for communicating with the Qserv worker services

--- core/modules/replica/replica_qserv_notify.cc
+++ core/modules/replica/replica_qserv_notify.cc
@@ -1,23 +1,24 @@
-#include "proto/worker.pb.h"
-#include "replica/CmdParser.h"
-#include "replica_core/BlockPost.h"
-
-#include "XrdSsi/XrdSsiProvider.hh"
-#include "XrdSsi/XrdSsiRequest.hh"
-#include "XrdSsi/XrdSsiService.hh"
-
-#include <algorithm>
-#include <memory>       // std::unique_ptr
+// System header
 #include <iostream>
-#include <iterator>
 #include <stdexcept>
 #include <string>
 
+// Third party headers
+#include "XrdSsi/XrdSsiProvider.hh"
+#include "XrdSsi/XrdSsiService.hh"
+
+// Qserv headers
+#include "proto/worker.pb.h"
+#include "replica/CmdParser.h"
+#include "replica_core/BlockPost.h"
+#include "replica_core/ReloadChunkListQservRequest.h"
+#include "replica_core/TestEchoQservRequest.h"
+
+/// This C++ symbol is provided by the SSI shared library
 extern XrdSsiProvider* XrdSsiProviderClient;
 
-namespace proto = lsst::qserv::proto;
-namespace r     = lsst::qserv::replica;
-namespace rc    = lsst::qserv::replica_core;
+namespace r  = lsst::qserv::replica;
+namespace rc = lsst::qserv::replica_core;
 
 namespace {
 
@@ -26,98 +27,10 @@ namespace {
 std::string serviceProviderLocation;
 std::string workerResourceName;
 
-class QServWorkerRequest : public XrdSsiRequest {
-
-public:
-
-    QServWorkerRequest() {
-        proto::WorkerCmdMsg msg;
-        msg.set_cmd(proto::WorkerCmdMsg::RELOAD_CHUNK_LIST);
-        msg.SerializeToString(&_msg);
-    }
-
-    ~QServWorkerRequest() override {}
-
-    char* GetRequest (int& dlen) override { dlen = _msg.size(); return const_cast<char*>(_msg.data()); }
-
-    bool ProcessResponse (const XrdSsiErrInfo&  eInfo,
-                          const XrdSsiRespInfo& rInfo) override {
-        if (eInfo.hasError()) {
-            std::cerr << "QServWorkerRequest::ProcessResponse  ** FAILED **, error: " << rInfo.eMsg << std::endl;
-            return false;
-        }
-        std::cout
-            << "QServWorkerRequest::ProcessResponse"
-            << "  eInfo.rType: " << rInfo.rType << "(" << rInfo.State() << ")"
-            << ", eInfo.blen: " << rInfo.blen << std::endl;
-
-        switch (rInfo.rType) {
-
-            case XrdSsiRespInfo::isData:
-            case XrdSsiRespInfo::isStream:
-
-                std::cout
-                    << "QServWorkerRequest::ProcessResponse  ** requesting response data **" << std::endl;
-
-                GetResponseData(_buf, _bufSize);
-                return true;
-        
-            default:
-                return false;
-        }
-    }
-
-    PRD_Xeq ProcessResponseData(const XrdSsiErrInfo& eInfo,
-                                char*                buff,
-                                int                  blen,
-                                bool                 last) override {
-
-        std::cerr
-            << "QServWorkerRequest::(PRD_Xeq)ProcessResponseData"
-            << "  eInfo.isOK(): " << (eInfo.isOK() ? "yes" : "no");
-
-        if (!eInfo.isOK()) {
-            std::cerr
-                << ", eInfo.Get(): "    << eInfo.Get()
-                << ", eInfo.GetArg(): " << eInfo.GetArg()
-                << std::endl;
-        } else {
-            std::cerr
-                << ", blen: " << blen
-                << ", last: " << (last ? "yes" : "no")
-                << std::endl;
-            proto::WorkerCmdReply reply;
-            reply.ParseFromString(std::string(buff, blen));
-
-            std::cout << "status returned in the reply: " << proto::WorkerCmdReply_Status_Name(reply.status()) << std::endl;
-            //std::copy(buff, buff + blen, std::ostream_iterator<char>(std::cout));
-
-            if (last) Finished();
-            else      GetResponseData(_buf, _bufSize);  // keep receiving results
-        }
-        return PRD_Normal;
-    }
-
-
-private:
-
-    /// Message to be sent
-    std::string _msg;
-
-    /// Length of the response buffer
-    static int const _bufSize{1024};
-
-    /// Buffer for response data
-    char _buf[_bufSize];
-};
-
-
 int test () {
 
-    //std::unique_ptr<char> recordPtr (new char[recordSizeBytes]);
-
-    XrdSsiErrInfo  errInfo;
-    XrdSsiService* serviceProvider =
+    XrdSsiErrInfo errInfo;
+    auto serviceProvider =
         XrdSsiProviderClient->GetService(errInfo,
                                          serviceProviderLocation);
 
@@ -129,15 +42,35 @@ int test () {
     }
     std::cout << "connected to service provider at: " << serviceProviderLocation << std::endl;
 
-    XrdSsiRequest* request = new QServWorkerRequest();
-    XrdSsiResource resource(workerResourceName);
+    XrdSsiResource resource (workerResourceName);
 
-    serviceProvider->ProcessRequest(*request, resource);
-
-    rc::BlockPost blockPost (1000, 5000);
+    rc::BlockPost blockPost (1000, 2000);
     while (true) {
-        blockPost.wait();
-        std::cout << "HEARTBEAT" << std::endl;
+
+        auto request1 =
+            new rc::ReloadChunkListQservRequest (
+                    [] (bool success,
+                        rc::ReloadChunkListQservRequest::ChunkCollection const& added,
+                        rc::ReloadChunkListQservRequest::ChunkCollection const& removed) {
+                        std::cout << "# chunks added:   " << added.size()   << std::endl;
+                        std::cout << "# chuks  removed: " << removed.size() << std::endl;
+                    });
+        serviceProvider->ProcessRequest(*request1, resource);
+
+        auto request2 =
+            new rc::TestEchoQservRequest (
+                    "12345678",
+                    [] (bool success,
+                        std::string const& sent,
+                        std::string const& received) {
+                        std::cout << "# value sent:     " << sent     << std::endl;
+                        std::cout << "# value received: " << received << std::endl;
+                    });
+        serviceProvider->ProcessRequest(*request2, resource);
+
+        blockPost.wait(500);
+
+        break;
     }
 
     return 0;
